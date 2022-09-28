9c76dc273805ccc04b991d20f122b71cb5fb7693
lucastheldl <lucastheldl@gmail.com>
2022-09-28 14:21:03 -0300

    Centering post image div

--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -5,7 +5,9 @@ import { Link } from "react-router-dom";
 const PostDetail = ({ post }) => {
   return (
     <div className={styles.post_detail}>
-      <img src={post.image} alt={post.title} />
+      <div className={styles.img_container}>
+        <img src={post.image} alt={post.title} />
+      </div>
 
       <h2>{post.title}</h2>
 
